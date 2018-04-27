a043d90c612bb04b187906bfb52812072aa3112a
Jordan Mynes <mynesjordan@gmail.com>
2018-04-27 18:48:07 -0500

    Beginnings of a working script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup
+* Install node packages
+* Define API keys as constants (I have them set in .bashrc, trying to get them set in .env for dotenv, as this is expected behavior):
+  * BINANCEPUB=apikeyhere
+  * BINANCESEC=secretkey
---
+++ script.js
@@ -0,0 +1,78 @@
+require('dotenv').config()
+const binance = require('node-binance-api');
+binance.options({
+  APIKEY: process.env.BINANCEPUB,
+  APISECRET: process.env.BINANCESEC,
+  useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+  // Toggle below when ready!
+  //test: true // If you want to use sandbox mode where orders are simulated
+});
+
+console.log("Issue a command");
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+  switch(d.toString().trim()) {
+    /*
+      List available commands and any other documentation relevant to a new user
+    */
+    case '?':
+    case 'help':
+    case 'list':
+    case 'commands':
+      console.log("You can use the commands: open, balance, or quit")
+    break;
+
+    /*
+      See all balances, no matter how small.
+      Also, display USDT and BTC while we're still testing.
+    */
+    case 'balances':
+    case 'balance':
+    case 'b':
+      binance.balance((error, balances) => {
+        console.log("balances()", balances);
+        console.log("USDT balance: ", balances.USDT.available);
+        console.log("BTC balance: ", balances.BTC.available);
+      });
+    break;
+
+    /*
+      List all open orders
+    */
+    case 'open':
+    case 'orders':
+    case 'o':
+      binance.openOrders(false, (error, openOrders) => {
+        console.log("openOrders()", openOrders);
+      });
+    break;
+
+    /*
+      Second piece of ticker (USDT) is the trading pair
+      e.g. XMRBTC means BTC market, XMR
+    */
+    case 'cancel':
+      binance.cancelOrders("BTCUSDT", (error, response, symbol) => {
+        console.log(symbol+" cancel response:", response);
+      });
+    break;
+
+    /*
+      Exit the program
+    */
+    case 'quit':
+    case 'q':
+    case 'exit':
+      process.exit(0)
+    break;
+
+    /*
+
+    */
+    default:
+      console.log("Command does not exist or failed. Please type help for a list of commands.")
+    break;
+  }
+});
