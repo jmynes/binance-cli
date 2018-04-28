1dbf9b71cbd737f3b3da9ae38298f72a34cef21b
Jordan Mynes <mynesjordan@gmail.com>
2018-04-27 19:07:45 -0500

    Moved code into functions, moved documentation and standardized

--- script.js
+++ script.js
@@ -8,67 +8,89 @@ binance.options({
   //test: true // If you want to use sandbox mode where orders are simulated
 });
 
+/*
+  List available commands and any other documentation relevant to a new user
+*/
+function help() {
+  console.log("You can use the commands: balances, cancel, help, open, price, or quit");
+}
+
+/*
+  See all balances, no matter how small.
+  Also, display USDT and BTC while we're still testing.
+*/
+function balance() {
+  binance.balance((error, balances) => {
+    console.log("balances()", balances);
+    console.log("USDT balance: ", balances.USDT.available);
+    console.log("BTC balance: ", balances.BTC.available);
+  });
+}
+
+/*
+  Second piece of ticker (USDT) is the trading pair
+  e.g. XMRBTC means BTC market, XMR
+*/
+function cancel() {
+  binance.cancelOrders("BTCUSDT", (error, response, symbol) => {
+    console.log(symbol+" cancel response:", response);
+  });
+}
+
+/*
+  List all open orders
+*/
+function open_orders() {
+  binance.openOrders(false, (error, openOrders) => {
+    console.log("openOrders()", openOrders);
+  });
+}
+
+/*
+  Get current price of asset (in our case, BNB priced in BTC)
+*/
+function price() {
+  binance.prices('BNBBTC', (error, ticker) => {
+    console.log("Price of BNB: ", ticker.BNBBTC);
+  });
+}
+
 console.log("Issue a command");
 
 var stdin = process.openStdin();
 
 stdin.addListener("data", function(d) {
   switch(d.toString().trim()) {
-    /*
-      List available commands and any other documentation relevant to a new user
-    */
-    case '?':
     case 'help':
     case 'list':
     case 'commands':
-      console.log("You can use the commands: balances, cancel, help, open, price, or quit");
+    case '?':
+    case 'h':
+      help();
     break;
 
-    /*
-      See all balances, no matter how small.
-      Also, display USDT and BTC while we're still testing.
-    */
-    case 'balances':
     case 'balance':
+    case 'balances':
     case 'b':
-      binance.balance((error, balances) => {
-        console.log("balances()", balances);
-        console.log("USDT balance: ", balances.USDT.available);
-        console.log("BTC balance: ", balances.BTC.available);
-      });
+      balance();
     break;
 
-    /*
-      Second piece of ticker (USDT) is the trading pair
-      e.g. XMRBTC means BTC market, XMR
-    */
     case 'cancel':
     case 'c':
-      binance.cancelOrders("BTCUSDT", (error, response, symbol) => {
-        console.log(symbol+" cancel response:", response);
-      });
+      cancel();
     break;
 
-    /*
-      List all open orders
-    */
     case 'open':
     case 'orders':
     case 'o':
-      binance.openOrders(false, (error, openOrders) => {
-        console.log("openOrders()", openOrders);
-      });
+      open_orders();
     break;
 
-    /*
-      Get current price of asset (in our case, BNB priced in BTC)
-    */
     case 'price':
     case 'p':
-      binance.prices('BNBBTC', (error, ticker) => {
-        console.log("Price of BNB: ", ticker.BNBBTC);
-      });
+      price();
     break;
+
     /*
       Exit the program
     */
@@ -79,7 +101,7 @@ stdin.addListener("data", function(d) {
     break;
 
     /*
-
+      Inform the user of an error
     */
     default:
       console.log("Command does not exist or failed. Please type help for a list of commands.")
